4d499bb3e35b2a476e1dbc842bd0601495134082
yuliana <yuli.mejiap@gmail.com>
2025-05-20 18:04:47 -0500

    Se modifica el nombre al formulario

--- src/components/pages/Login/Login.jsx
+++ src/components/pages/Login/Login.jsx
@@ -5,7 +5,7 @@ export function Login() {
                 <section className="row justify-content-center">
                     <section className="col-12 col-md-6">
                         <form className="p-5 border rounded shadow">
-                            <h3 className="text-center mb-4">Formulario de Asistencia</h3>
+                            <h3 className="text-center mb-4">Formulario de Asistencias</h3>
                             <input type="text" className="form-control mb-3" placeholder="Estudiante" />
                             <input type="text" className="form-control mb-3" placeholder="Grupo" />
                             <input type="date" className="form-control mb-3" placeholder="Fecha" />
